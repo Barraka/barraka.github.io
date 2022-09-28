2fa88635dc0a1dbea52e76733d6eebe32cb33fc8
Emmanuel <girault.e@gmail.com>
2022-09-28 08:26:47 +0200

    Modified files, didn't work the first time

--- src/resetsize.js
+++
@@ -1,5 +0,0 @@
-export default function resetsize() {
-    let title = document.querySelector('.title');
-    let titlebg = document.querySelector('.titlebg');
-    titlebg.style.height=title.clientHeight+'px';
-}
